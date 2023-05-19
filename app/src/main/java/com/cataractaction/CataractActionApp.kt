e853b5247c90f058b13be0e6abb4e259fd279895
Rivan Albani Ray <rivanalbaniray88@gmail.com>
2023-05-19 08:39:52 +0700

    setup

---
+++ app/src/main/java/com/cataractaction/CataractActionApp.kt
@@ -0,0 +1,7 @@
+package com.cataractaction
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CataractActionApp() {
+}
---
+++ app/src/main/java/com/cataractaction/ui/theme/Theme.kt
@@ -0,0 +1,51 @@
+package com.cataractaction.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorScheme = darkColors(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    primaryVariant = Pink80
+)
+
+private val LightColorScheme = lightColors(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    primaryVariant = Pink40
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun CataractActionTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+
+}
